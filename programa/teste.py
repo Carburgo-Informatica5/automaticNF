a4720e19091d909004950fe5dfd6a79de0779f7b
dealerautomationbrasil <dealerautomationbrasil@gmail.com>
2024-11-21 11:31:55 -0300

    atualização_automaticNF - 21.11.2024 - CARBURGO

--- programa/teste.py
+++ programa/teste.py
@@ -12,6 +12,11 @@ modelo = "55"
 
 gui.PAUSE = 0.5
 
+
+gui.alert("O código vai começar. Não utilize nada do computador até o código finalizar!")
+
+
+# Acesso a notas fiscais de despesas
 time.sleep(5)
 gui.press("alt")
 gui.press("right", presses=6)
@@ -21,43 +26,35 @@ gui.press("down", presses=2)
 gui.press("enter")
 time.sleep(5)
 
-posicao_transacao = gui.locateCenterOnScreen("C:/Users/VAS MTZ/Desktop/Caetano Apollo/automaticNF/programa/assets/transacao.jpg", confidence=0.8)
-posicao_nmr_nota = gui.locateCenterOnScreen("C:/Users/VAS MTZ/Desktop/Caetano Apollo/automaticNF/programa/assets/nmr_nota.jpg", confidence=0.8)
-posicao_serie = gui.locateCenterOnScreen("C:/Users/VAS MTZ/Desktop/Caetano Apollo/automaticNF/programa/assets/serie.jpg", confidence=0.8)
-posicao_contador = gui.locateCenterOnScreen("C:/Users/VAS MTZ/Desktop/Caetano Apollo/automaticNF/programa/assets/contador.jpg", confidence=0.8)
-
-gui.click(posicao_transacao, duration=0.1)
-gui.press("down")
-gui.press("enter")
-gui.click(posicao_nmr_nota, duration=0.1)
+# Preenche campos de nmr da nota, série, transação, contador e cliente
+gui.press("tab")
 gui.write(nmr_nota)
-gui.press("enter")
-gui.click(posicao_serie, duration=0.1)
+gui.press("tab")
 gui.write(serie)
-gui.press("enter")
-gui.click(posicao_contador, duration=0.1)
+gui.press("tab")
+gui.press("down")
+gui.press("tab")
 gui.write(contador)
-gui.press("enter")
-
-posicao_mao = gui.locateCenterOnScreen("C:/Users/VAS MTZ/Desktop/Caetano Apollo/automaticNF/programa/assets/mao.jpg", confidence=0.8)
-gui.click(posicao_mao, duration=0.1)
-
-posicao_avancado = gui.locateCenterOnScreen("C:/Users/VAS MTZ/Desktop/Caetano Apollo/automaticNF/programa/assets/avancado.jpg", confidence=0.8)
-gui.click(posicao_avancado, duration=0.1)
-gui.write(cnpj_eminente)
-gui.press(["enter", "enter", "tab", "tab"])
-
-posicao_entrada= gui.locateCenterOnScreen("C:/Users/VAS MTZ/Desktop/Caetano Apollo/automaticNF/programa/assets/data_entrada.jpg", confidence=0.8)
-gui.click(posicao_entrada, clicks=2, duration=0.1)
-gui.press("tab")
-gui.write(data_emi)
-
-posicao_modelo = gui.locateCenterOnScreen("C:/Users/VAS MTZ/Desktop/Caetano Apollo/automaticNF/programa/assets/modelo.jpg", confidence=0.8)
-gui.click(posicao_modelo, duration=0.1)
-gui.write(modelo)
 gui.press("tab")
 
-posicao_chave = gui.locateCenterOnScreen("C:/Users/VAS MTZ/Desktop/Caetano Apollo/automaticNF/programa/assets/chaveAcesso.jpg", confidence=0.8)
-gui.click(posicao_chave, duration=0.1)
-gui.write(chave_acesso)
-gui.press("enter")
+
+
+# posicao_avancado = gui.locateCenterOnScreen("C:/Users/VAS MTZ/Desktop/Caetano Apollo/automaticNF/programa/assets/avancado.jpg", confidence=0.8)
+# gui.click(posicao_avancado, duration=0.1)
+# gui.write(cnpj_eminente)
+# gui.press(["enter", "enter", "tab", "tab"])
+
+# posicao_entrada= gui.locateCenterOnScreen("C:/Users/VAS MTZ/Desktop/Caetano Apollo/automaticNF/programa/assets/data_entrada.jpg", confidence=0.8)
+# gui.click(posicao_entrada, clicks=2, duration=0.1)
+# gui.press("tab")
+# gui.write(data_emi)
+
+# posicao_modelo = gui.locateCenterOnScreen("C:/Users/VAS MTZ/Desktop/Caetano Apollo/automaticNF/programa/assets/modelo.jpg", confidence=0.8)
+# gui.click(posicao_modelo, duration=0.1)
+# gui.write(modelo)
+# gui.press("tab")
+
+# posicao_chave = gui.locateCenterOnScreen("C:/Users/VAS MTZ/Desktop/Caetano Apollo/automaticNF/programa/assets/chaveAcesso.jpg", confidence=0.8)
+# gui.click(posicao_chave, duration=0.1)
+# gui.write(chave_acesso)
+# gui.press("enter")
